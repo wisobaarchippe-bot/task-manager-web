edf2377c99f68963d29c971d1f810e4bd82f2ba6
Archippe Iluba <wisobaarchippe@gmail.com>
2025-11-15 12:58:28 +0100

    Add Dashboard page component

---
+++ frontend/src/pages/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TaskList from "../components/TaskList";
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard - Mes tâches</h1>
+      <TaskList />
+    </div>
+  );
+}
+
+export default Dashboard;
